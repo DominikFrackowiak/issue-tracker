d1c15c8064c1446850aabbcb4d402cd1672a6fd1
DominikFrackowiak <dominikfrac@gmail.com>
2024-02-04 20:56:55 +0100

    navigation done and styled

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,30 +1,42 @@
+'use client'
+
+import { usePathname } from 'next/navigation'
+
 import Link from 'next/link'
 
 import { AiFillBug } from 'react-icons/ai'
 
+const navLinks = [
+	{
+		title: 'Dashboard',
+		url: '/dashboard',
+	},
+	{
+		title: 'Issues',
+		url: '/issues',
+	},
+]
+
 export default function NavBar() {
+	const currentPath = usePathname()
+	console.log(currentPath)
 	return (
 		<nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
 			<Link href='/'>
 				<AiFillBug />
 			</Link>
 			<ul className='flex space-x-6'>
-				<li>
-					<Link
-						className='text-zinc-500 hover:text-zinc-800 transition-colors'
-						href='/dashboard'
-					>
-						Dashboard
+				{navLinks.map(link => (
+					<Link key={link.title} href={link.url}>
+						<li
+							className={`${
+								link.url === currentPath ? 'text-zinc-900' : ' text-zinc-500'
+							} hover:text-zinc-800 transition-color`}
+						>
+							{link.title}
+						</li>
 					</Link>
-				</li>
-				<li>
-					<Link
-						className='text-zinc-500 hover:text-zinc-800 transition-colors'
-						href='/issues'
-					>
-						Issues
-					</Link>
-				</li>
+				))}
 			</ul>
 		</nav>
 	)
